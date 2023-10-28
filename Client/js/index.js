71d084282f7af7f631e4fec39fac86632448b6c7
Abhilash <abhilashjha335@gmail.com>
2023-10-29 02:14:06 +0530

    first commit chatapp

---
+++ Client/js/index.js
@@ -0,0 +1,36 @@
+const socket = io('ws://localhost:5000')
+const inputMsg = document.getElementById('inputMsg');
+const form = document.getElementById('textMsg');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const msg = inputMsg.value;
+    if(inputMsg.value){
+      socket.emit('message', msg);
+      inputMsg.value = "";
+    }
+
+    inputMsg.focus();
+})
+
+function msgFormat(msg){
+    let li = document.createElement('li');
+    li.className = 'post-msg';
+    li.innerHTML = `
+        <div class="userInfo">
+            <span class="userName">User</span>
+            <span class="time">5:30 P.M</span>
+        </div>
+        <div class="usermsg">
+            <img src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=740&t=st=1698409334~exp=1698409934~hmac=9695f95e0e9113d03f1557ff5373143a6b68e7c7f8b742b7acf9a2e07b5526ac"
+                alt="user image" class="image" width="50" height="50">
+            <span id="text-msg">${msg}</span>
+        </div>
+    `
+    const ul = document.querySelector('ul');
+    ul.appendChild(li);
+}
+
+socket.on('message', (data) => {
+    msgFormat(data);
+})
---
+++ Server/app.js
@@ -0,0 +1,25 @@
+import { Server } from "socket.io";
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const chatServer = app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
+
+const io = new Server(chatServer, {
+    cors: {
+        origin: process.env.NODE_ENV === 'production' ? false : ['http://localhost:5500', 'http://127.0.0.1:5500']
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('A connection has been established successfully!');
+    socket.emit('message', `Welcome, ${(socket.id).substring(0,5)} in chatter talk`);
+    socket.broadcast.emit('message', `User ${(socket.id).substring(0,5)} is entered in the chat!`)
+
+    socket.on('message', (data) => {
+      io.emit('message', data);
+    })
+
+   socket.on('disconnect', () => console.log("A user has been disconnected from app!"))
+})

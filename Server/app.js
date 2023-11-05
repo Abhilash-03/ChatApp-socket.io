dda6bc2f7ed32a18154cf4990f331586ab35aad6
Abhilash <abhilashjha335@gmail.com>
2023-11-05 11:22:31 +0530

    add vercel.json

--- Server/app.js
+++ Server/app.js
@@ -5,6 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const ADMIN = 'Bot';
 
+app.get('/', (req, res) => res.send("Welcome to chatter talk"));
+
 const chatServer = app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
 
 const io = new Server(chatServer, {
